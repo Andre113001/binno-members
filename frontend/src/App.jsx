6edc103320953ca2cf3dc0d8e8762949db1985f8
Andre113001 <1130marcusa@gmail.com>
2023-08-26 18:25:31 +0800

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+
+return (
+ 
+  <>
+    <div className="lg:bg-blue-200 sm:bg-blue-300">
+      <h1 className='text-3xl font-bold underlinw'>Hello  World
+      </h1>
+    </div>
+  </>
+
+  )
+}
+
+export default App
